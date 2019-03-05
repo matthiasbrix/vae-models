39e0cc2fa3d0706a6fc32a9ab3faca531de3cecd
Matthias Brix <brixmatthias@gmail.com>
2019-03-05 11:49:38 +0100

    more stuff

--- examples/vae-example/main.py
+++ examples/vae-example/main.py
@@ -2,15 +2,18 @@ import torch
 import torch.utils.data
 import torch.nn as nn
 import torch.nn.functional as F
-import torchvision.datasets
-import torchvision.transforms
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
 # TODO: Optim algorithm
 # TODO: loss function
 
-train_loader = torch.utils.data.DataLoader()
+mnist_train = datasets.MNIST(root="../data", train=True, transform=transforms.ToTensor(), download=True)
+mnist_test = datasets.MNIST(root="../data", train=False, transform=transforms.ToTensor(), download=False)
+train_loader = torch.utils.data.DataLoader(dataset=mnist_train, batch_size=1, shuffle=False)
+test_loader = torch.utils.data.DataLoader(dataset=mnist_test, batch_size=1, shuffle=False)
 
-test_loader = torch.utils.data.DataLoader()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class VAE(nn.Module):
     
@@ -31,4 +34,7 @@ def test():
 
 # call from here
 if __name__ == "__main__":
-    pass
+    print(type(mnist_train))
+    print(type(mnist_test))
+    print(train_loader)
+    model = VAE().to(device)
