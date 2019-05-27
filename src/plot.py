fixed few plot bugs; added scaling+rotation; refactored storing y/z space labels; made latent space plots more modular in terms of labels

src/plot.py
@@ -4,6 +4,12 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import scipy.stats as stats
 
+DATASETS = {
+    "MNIST": "MNIST",
+    "FF": "Frey Faces",
+    "LFW": "Labeled Faces in the Wild"
+}
+
 # Auxiliary function for saving nice plots
 # Saves figure without white space borders
 # from https://fengl.org/2014/07/09/matplotlib-savefig-without-borderframe/
@@ -21,7 +27,6 @@ def _save_plot_fig(solver, data, cm, name):
         solver.data_loader.dataset + "_z=" + str(solver.z_dim) + ".png", dpi=height)
     plt.close()
 
-# TODO: breaks when too few epochs
 def _xticks(ls, ticks_rate):
     labels = np.arange(1, len(ls)+2, (len(ls)//ticks_rate))
     labels[1:] -= 1
@@ -29,40 +34,48 @@ def _xticks(ls, ticks_rate):
     return labels.astype(int)
 
 # Plotting train and test losses
-def plot_losses(solver, ticks_rate):
+def plot_losses(solver):
     train_loss_history = solver.train_loss_history["train_loss_acc"]
     test_loss_history = solver.test_loss_history
     plt.figure(figsize=(5, 3))
     plt.plot(np.arange(1, len(train_loss_history)+1), train_loss_history, label="Train")
     plt.plot(np.arange(1, len(solver.test_loss_history)+1), test_loss_history, label="Test")
+    ticks_rate = 4 if len(train_loss_history) >= 4 else len(train_loss_history)
     plt.xticks(_xticks(train_loss_history, ticks_rate))
-    plt.title("Loss on data set {}, dim(z)={}".format(solver.data_loader.dataset, solver.z_dim)) # marginal likelihood log p(x)
+    plt.title("Loss on data set {}, dim(z)={}".format(DATASETS[solver.data_loader.dataset], solver.z_dim)) # marginal likelihood log p(x)
     plt.xlabel("epoch")
     plt.ylabel("loss")
     plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.4),
             fancybox=True, shadow=True, ncol=5)
     plt.subplots_adjust(left=0.2, right=0.85, top=0.9, bottom=0.25)
     plt.savefig(solver.data_loader.result_dir + "/" + "plot_losses_" + \
-        solver.data_loader.dataset + "_z=" + str(solver.z_dim) + ".png")
+        DATASETS[solver.data_loader.dataset] + "_z=" + str(solver.z_dim) + ".png")
     plt.show()
 
 # Plotting histogram of the latent space's distribution, given the computed \mu and \sigma
 # TODO: could be done better? Maybe just have 1 column and then "num_plots" rows
 def plot_gaussian_distributions(solver):
-    f, axarr = plt.subplots(2, 2, figsize=(8, 6))
     x = np.linspace(-5, 5, 5000)
     idx_x = 0
     idx_y = 0
     epochs = len(solver.train_loss_history["train_loss_acc"]) # in case run was canceled
-    if epochs % 2 != 0:
-        plots = np.arange(1, epochs+1, np.ceil(epochs/4)+1).astype(int)
-        plots[2:] += 1
-        plots[-1] = epochs
-    else:
-        plots = np.arange(1, epochs+1, np.ceil(epochs/4)).astype(int)
-        plots[1:] -= 1
-        plots[-1] = epochs
-    f.subplots_adjust(hspace=0.5, wspace=0.3)
+    #if epochs % 2 != 0:
+    #    plots = np.arange(1, epochs+1, np.ceil(epochs/4)+1).astype(int)
+    #    plots[2:] += 1
+    #    plots[-1] = epochs
+    #else:
+    #    plots = np.arange(1, epochs+1, np.ceil(epochs/4)).astype(int)
+    #    plots[1:] -= 1
+    #    plots[-1] = epochs
+    plots = np.arange(1, epochs+1, np.ceil(epochs/4)).astype(int)
+    plots[-1] = epochs
+    if epochs == 1:
+        f, axarr = plt.subplots(1, 1, figsize=(8, 2))
+    if epochs == 2 or epochs == 3:
+        f, axarr = plt.subplots(1, 2, figsize=(8, 4))
+    if epochs >= 4:
+        f, axarr = plt.subplots(2, 2, figsize=(8, 6))
+        f.subplots_adjust(hspace=0.5, wspace=0.3)
     ys = []
     for idx in plots:
         i = idx-1
@@ -74,9 +87,14 @@ def plot_gaussian_distributions(solver):
         y = (1 / (np.sqrt(2 * np.pi * var_z))) * \
                 (np.power(np.e, -(np.power((x - mu_z), 2) / (2 * var_z))))
         ys.append(np.max(y))
-        axarr[idx_x, idx_y].plot(x, y, label="Latent distr.")
-        axarr[idx_x, idx_y].plot(x, stats.norm.pdf(x, 0, 1), label="Standard\nGaussian distr.")
-        axarr[idx_x, idx_y].set_title("epoch %d\nμ(z)=%.4f, σ^2(z)=%.4f" % (epoch, mu_z, var_z))
+        if epochs <= 3:
+            axarr[idx_y].plot(x, y, label="Latent distr.")
+            axarr[idx_y].plot(x, stats.norm.pdf(x, 0, 1), label="Standard\nGaussian distr.")
+            axarr[idx_y].set_title("epoch %d\nμ(z)=%.4f, σ^2(z)=%.4f" % (epoch, mu_z, var_z))
+        else:
+            axarr[idx_x, idx_y].plot(x, y, label="Latent distr.")
+            axarr[idx_x, idx_y].plot(x, stats.norm.pdf(x, 0, 1), label="Standard\nGaussian distr.")
+            axarr[idx_x, idx_y].set_title("epoch %d\nμ(z)=%.4f, σ^2(z)=%.4f" % (epoch, mu_z, var_z))
         if idx_x == idx_y or idx_x > idx_y:
             idx_y += 1
         else: # idx_x < idx_y
@@ -101,21 +119,25 @@ def plot_gaussian_distributions(solver):
             file_res.write(str(epoch) + "," + str(np.around(np.array(varmu_z), 4)) + "," + str(np.around(np.array(expected_var_z.item()), 4)))
             file_res.write("\n")
 
+    if epochs <= 3:
+        ax = axarr.flatten()[0]
+    if epochs >= 4:
+        ax = axarr.flatten()[2]
     f.subplots_adjust(top=0.9, left=0.1, right=0.9, bottom=0.2)
-    ax = axarr.flatten()[2]
     ax.legend(loc='upper center', bbox_to_anchor=(1.2, -0.25),
-              fancybox=True, shadow=True, ncol=5)
+            fancybox=True, shadow=True, ncol=5)
 
-    plt.savefig(solver.data_loader.result_dir + "/" + "plot_gaussian_" + \
+    plt.savefig(solver.data_loader.result_dir + "/" + "plot_gaussian_" +\
         solver.data_loader.dataset + "_z=" + str(solver.z_dim) + ".png")
 
 # Plot the reconstruction loss and KL divergence in two separate plots
-def plot_rl_kl(solver, ticks_rate):
+def plot_rl_kl(solver):
     rls = solver.train_loss_history["recon_loss_acc"]
     kls = solver.train_loss_history["kl_diverg_acc"]
     x = np.arange(1, len(kls)+1)
     plt.figure(figsize=(4.5, 5))
 
+    ticks_rate = 4 if len(rls) >= 4 else len(rls)
     plt.subplot(2, 1, 1)
     plt.plot(x, rls)
     plt.xticks(_xticks(rls, ticks_rate))
@@ -137,16 +159,17 @@ def plot_rl_kl(solver, ticks_rate):
 
 # Plot the latent space as scatter plot with and without labels
 # TODO: n_classes is not that generic? Should be an arg?
-def plot_latent_space(solver, space, var, labels=None):
+def plot_latent_space(solver, space, var, title=None, labels=None):
     plt.figure(figsize=(9, 7))
     if solver.data_loader.with_labels:
         plt.scatter(space[:, 0], space[:, 1], s=10, c=labels.tolist(), cmap=plt.cm.get_cmap("Paired", solver.data_loader.n_classes))
-        plt.colorbar()
+        clb = plt.colorbar()
+        clb.ax.set_title(title)
     else:
         plt.scatter(space[:, 0], space[:, 1], s=10, cmap="Paired")
     plt.xlabel("{}_1".format(var))
     plt.ylabel("{}_2".format(var))
-    plt.title("Latent space q({}) on data set {} after {} epochs".format(var, solver.data_loader.dataset, solver.epochs))
+    plt.title("Latent space q({}) on data set {} after {} epochs".format(var, DATASETS[solver.data_loader.dataset], solver.epochs))
     plt.savefig(solver.data_loader.result_dir + "/plot_" + str(var) + "_space_" \
         + solver.data_loader.dataset + "_z=" + str(solver.z_dim) + ".png")
 
@@ -221,6 +244,21 @@ def plot_with_fixed_z(solver, n_rows, n_cols, cm, fig_size=(6, 6)):
     plt.imshow(figure, cmap=cm)
     plt.show()
     _save_plot_fig(solver, figure, cm=cm, name="fixed_z_all_labels")
+
+# TODO
+def plot_prepro_params_distribution(counts):
+    indices = list(counts.keys())
+    plt.figure(figsize=(20, 20))
+    plt.bar(indices, counts.values())
+    plt.xlabel("theta_1")
+    plt.ylabel("Count")
+    plt.xticks(indices, fontsize=5, rotation=30)
+    plt.title("Angle distribution for y_t")
+    plt.show()
+
+# TODO: make a subplot for each class on how much it has been rotated
+def plot_prepro_params_distribution_categories():
+    pass
 
 # takes only numpy array in, so mainly for testing puposes
 def plot_faces_grid(n, n_cols, solver, fig_size=(10, 8)):

src/preprocessing.py
@@ -30,16 +30,23 @@ class Preprocessing():
         self.rotate = thetas is not None
         self.scale = scales is not None
         self.data_loader = data_loader
+        self.prepro_params = {}
         if self.rotate:
             self.theta_range_1, self.theta_range_2 = [v for _, v in thetas.items()]
             # add 1's because it's exclusive when gen. random
             self.theta_range_1[1] += 1
             self.theta_range_2[1] += 1
+            self.prepro_params["theta_1"] = []
+            self.prepro_params["theta_diff"] = []
         if self.scale:
+            # find the max possible scale and set img dims to be that
             self.scale_range_1, self.scale_range_2 = scales["scale_1"], scales["scale_2"]
             max_scale = round(self.scale_range_1[1] + self.scale_range_2[1], 1)
             self.data_loader.img_dims = tuple([int(max_scale*x) for x in list(self.data_loader.img_dims)])
             self.data_loader.input_dim = np.prod(self.data_loader.img_dims)
+            self.prepro_params["scale_1"] = []
+            self.prepro_params["scale_diff"] = []
+        self.theta_1, self.theta_2, self.scale_1, self.scale_2 = None, None, None, None
 
     def _generate_angles(self):
         theta_1 = np.random.randint(*self.theta_range_1)
@@ -49,12 +56,14 @@ class Preprocessing():
     def _generate_scales(self):
         scale_1 = uniform(*self.scale_range_1)
         scale_2 = scale_1 + uniform(*self.scale_range_2)
+        if scale_1 <= 0 or scale_2 <= 0:
+            raise ValueError("One of the scales is <= 0!")
         return scale_1, scale_2
 
     def _rotate_batch(self, batch, angle):
         rotated = torch.zeros_like(batch)
         for i in range(batch.size(0)):
-            img = TF.rotate(transforms.ToPILImage()(batch[i].cpu()), angle)
+            img = TF.rotate(transforms.ToPILImage()(batch[i]), angle)
             rotated[i] = transforms.ToTensor()(img)
         return rotated
 
@@ -63,7 +72,7 @@ class Preprocessing():
         reshaped_dims = tuple([int(scale*x) for x in list((batch.size(2), batch.size(3)))])
         scaled = torch.zeros((batch.size(0), batch.size(1), *reshaped_dims))
         for i in range(batch.size(0)):
-            img = TF.resize(transforms.ToPILImage()(batch[i].cpu()), reshaped_dims)
+            img = TF.resize(transforms.ToPILImage()(batch[i]), reshaped_dims)
             scaled[i] = transforms.ToTensor()(img)
         if reshaped_dims < self.data_loader.img_dims:
             x, y = tuple([(x-x2) for (x, x2) in zip(self.data_loader.img_dims, reshaped_dims)])
@@ -74,27 +83,30 @@ class Preprocessing():
         scaled_batch = self._scale_batch(batch, scale)
         return self._rotate_batch(scaled_batch, angle)
 
-    def preprocess_batch(self, x):
-        res = {}
+    def save_params(self):
         if self.rotate:
-            theta_1, theta_2 = self._generate_angles()
-            x1 = self._rotate_batch(x, theta_1)
-            x2 = self._rotate_batch(x, theta_2)
-            theta_diff = theta_2 - theta_1
+            theta_diff = self.theta_2 - self.theta_1
+            self.prepro_params["theta_diff"].append(theta_diff)
+            self.prepro_params["theta_1"].append(self.theta_1)
+        if self.scale:
+            scale_diff = self.scale_2 - self.scale_1
+            self.prepro_params["scale_diff"].append(scale_diff)
+            self.prepro_params["scale_1"].append(self.scale_1)
+
+    def preprocess_batch(self, x):
+        if self.rotate and self.scale:
+            self.theta_1, self.theta_2 = self._generate_angles()
+            self.scale_1, self.scale_2 = self._generate_scales()
+            x1 = self._scale_rotate_batch(x, self.scale_1, self.theta_1)
+            x2 = self._scale_rotate_batch(x, self.scale_2, self.theta_2)
         elif self.scale:
-            scale_1, scale_2 = self._generate_scales()
-            x1 = self._scale_batch(x, scale_1)
-            x2 = self._scale_batch(x, scale_2)
-            scale_diff = scale_2 - scale_1
-        elif self.rotate and self.scale:
-            # TODO:
-            pass
+            self.scale_1, self.scale_2 = self._generate_scales()
+            x1 = self._scale_batch(x, self.scale_1)
+            x2 = self._scale_batch(x, self.scale_2)
+        elif self.rotate:
+            self.theta_1, self.theta_2 = self._generate_angles()
+            x1 = self._rotate_batch(x, self.theta_1)
+            x2 = self._rotate_batch(x, self.theta_2)
         else:
             raise ValueError("Prepro of batch failed")
-        if self.rotate:
-            res["theta_diff"] = theta_diff
-            res["theta_1"] = theta_1
-        if self.scale:
-            res["scale_diff"] = scale_diff
-            res["scale_1"] = scale_1
-        return x1.view(-1, self.data_loader.input_dim), x2.view(-1, self.data_loader.input_dim), res
+        return x1.view(-1, self.data_loader.input_dim), x2.view(-1, self.data_loader.input_dim)

src/solver.py
@@ -38,14 +38,13 @@ class Training(object):
 
     def _train_batch(self, epoch_metrics, x, y=None):
         y_space = None
-        result_prepro_params = None
         self.solver.optimizer.zero_grad()
         if self.solver.cvae_mode:
             x = x.view(-1, self.solver.data_loader.input_dim).to(self.solver.device)
             y = y.to(self.solver.device)
             decoded, mu_x, logvar_x, z_space = self.solver.model(x, y)
         elif self.solver.tdcvae_mode:
-            x_rot, x_next, result_prepro_params = self.solver.prepro.preprocess_batch(x)
+            x_rot, x_next = self.solver.prepro.preprocess_batch(x)
             x_rot, x_next = x_rot.to(self.solver.device), x_next.to(self.solver.device)
             decoded, x, mu_x, logvar_x, z_space, y_space = self.solver.model(x_rot, x_next)
         else:
@@ -57,35 +56,27 @@ class Training(object):
         self.solver.optimizer.step()
         epoch_metrics.compute_batch_train_metrics(loss.item(), reconstruction_loss,\
             kl_divergence, z_space, mu_x, logvar_x)
-        return z_space, y_space, result_prepro_params
+        return z_space, y_space
 
     def train(self, epoch, epoch_metrics):
         self.solver.model.train()
         for batch_idx, data in enumerate(self.solver.data_loader.train_loader):
             if self.solver.data_loader.with_labels:
                 x, y = data[0], data[1]
-                z_space, y_space, result_prepro_params = self._train_batch(epoch_metrics, x, y)
+                z_space, y_space = self._train_batch(epoch_metrics, x, y)
             else:
                 x = data
-                z_space, y_space, result_prepro_params = self._train_batch(epoch_metrics, x)
+                z_space, y_space = self._train_batch(epoch_metrics, x)
             # saving the z space, and y space if it's available
-            # TODO: DO THIS SMARTER!!! put into proc, and also the saving of y space labels...
             if epoch == self.solver.epochs and batch_idx != (len(self.solver.data_loader.train_loader)-1):
                 start = batch_idx*x.size(0)
                 end = (batch_idx+1)*x.size(0)
                 self.solver.z_space[start:end, :] = z_space.cpu().detach().numpy()
-                if y_space is not None:
-                    self.solver.y_space[start:end, :] = y_space.cpu().detach().numpy()
                 if not self.solver.tdcvae_mode and self.solver.data_loader.with_labels and y is not None:
                     self.solver.z_space_labels[start:end] = y.cpu().detach().numpy()
-                if self.solver.tdcvae_mode and self.solver.prepro.rotate and result_prepro_params["theta_diff"]:
-                    self.solver.z_space_labels[start:end] = np.repeat(result_prepro_params["theta_diff"], x.size(0))
-                if self.solver.tdcvae_mode and self.solver.prepro.rotate and result_prepro_params["theta_1"]:
-                    self.solver.y_space_labels[start:end] = np.repeat(result_prepro_params["theta_1"], x.size(0))
-                if self.solver.tdcvae_mode and self.solver.prepro.scale and result_prepro_params["scale_diff"]:
-                    self.solver.z_space_labels[start:end] = np.repeat(result_prepro_params["scale_diff"], x.size(0))
-                if self.solver.tdcvae_mode and self.solver.prepro.scale and result_prepro_params["scale_1"]:
-                    self.solver.y_space_labels[start:end] = np.repeat(result_prepro_params["scale_1"], x.size(0))
+                if y_space is not None:
+                    self.solver.y_space[start:end, :] = y_space.cpu().detach().numpy()
+                self.solver.prepro.save_params()
 
 class Testing(object):
     def __init__(self, solver):
@@ -96,7 +87,7 @@ class Testing(object):
             x = x.view(-1, self.solver.data_loader.input_dim).to(self.solver.device)
             decoded, mu_x, logvar_x, _ = self.solver.model(x, y)
         elif self.solver.tdcvae_mode:
-            x_rot, x_next, _ = self.solver.prepro.preprocess_batch(x)
+            x_rot, x_next = self.solver.prepro.preprocess_batch(x)
             x_rot, x_next = x_rot.to(self.solver.device), x_next.to(self.solver.device)
             decoded, x, mu_x, logvar_x, _, _ = self.solver.model(x_rot, x_next)
         else:
@@ -143,7 +134,6 @@ class Solver(object):
         self.z_space = np.zeros(((len(self.data_loader.train_loader)-1)*self.data_loader.batch_size, z_dim))
         self.z_space_labels = np.zeros((len(self.data_loader.train_loader)-1)*self.data_loader.batch_size)
         self.y_space = np.zeros(((len(self.data_loader.train_loader)-1)*self.data_loader.batch_size, z_dim))
-        self.y_space_labels = np.zeros((len(self.data_loader.train_loader)-1)*self.data_loader.batch_size)
         self.cvae_mode = cvae_mode
         self.tdcvae_mode = tdcvae_mode
         self.num_samples = num_samples
@@ -179,7 +169,8 @@ class Solver(object):
             elif self.tdcvae_mode:
                 z_sample = torch.randn(num_samples, self.z_dim).to(self.device)
                 x_t = iter(self.data_loader.train_loader).next()[0][:num_samples]
-                x_t, _, _ = self.prepro.preprocess_batch(x_t)
+                x_t, _ = self.prepro.preprocess_batch(x_t)
+                x_t = x_t.to(self.device)
                 sample = torch.cat((x_t, z_sample), dim=-1)
             else:
                 sample = torch.randn(num_samples, self.z_dim).to(self.device)
@@ -198,8 +189,9 @@ class Solver(object):
                 "batch_size: {}\n"\
                 "lr_scheduler: {}\n"\
                 "step_config: {}\n"\
-                .format(self.epochs, self.optimizer, self.beta, self.z_dim, self.data_loader.batch_size,\
-                    self.lr_scheduler, self.step_config)
+                .format(self.epochs, self.optimizer, self.beta, self.z_dim,\
+                    self.data_loader.batch_size, self.lr_scheduler,\
+                    self.step_config)
             if self.prepro:
                 if self.prepro.rotate:
                     params += "thetas: (theta_range_1: {}, theta_range_2: {})\n"\