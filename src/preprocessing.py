ad2c968fb99a08ec8678d3e5b92ec6aa4a0b8634
Matthias Brix <brixmatthias@gmail.com>
2019-08-12 10:04:13 +0200

    remove unnecesary code

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,13 +5,16 @@ import skimage as ski
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def generate_angles(theta_range_1, theta_range_2):
+def generate_angles(theta_range_1, theta_range_2, bound_rad=None):
     if theta_range_1 is None and theta_range_2 is None:
         theta_1 = -np.pi + np.random.uniform() * 2 * np.pi
-        theta_2 = theta_1 -np.pi/4 + np.random.uniform() * 2* np.pi/4
+        bound_rad = np.pi/4 if bound_rad is None else bound_rad
+        theta_2 = theta_1 -bound_rad + np.random.uniform() * 2 * bound_rad
     else:
-        theta_1 = np.around(uniform(*theta_range_1), decimals=2)
-        theta_2 = theta_1 + np.around(uniform(*theta_range_2), decimals=2)
+        theta_1 = uniform(*theta_range_1)
+        theta_2 = theta_1 + uniform(*theta_range_2)
+    theta_1 = np.around(theta_1, decimals=2)
+    theta_2 = np.around(theta_2, decimals=2)
     return theta_1, theta_2
 
 def generate_scales(scale_range_1, scale_range_2):
@@ -21,6 +24,8 @@ def generate_scales(scale_range_1, scale_range_2):
     else:
         scale_1 = np.around(uniform(*scale_range_1), decimals=2)
         scale_2 = scale_1 + np.around(uniform(*scale_range_2), decimals=2)
+    scale_1 = np.around(scale_1, decimals=2)
+    scale_2 = np.around(scale_2, decimals=2)
     return scale_1, scale_2
 
 # expecting a tensor (1, H, W) so not working on multuple channels.
@@ -44,25 +49,6 @@ def preprocess_batch_det(x, thetas, scales):
         preprocessed_x.append(preprocess_sample(x[i], theta=thetas[i], scale=(scales[i], scales[i])))
     preprocessed_x = np.stack(preprocessed_x, axis=0)
     return preprocessed_x
-
-# expecting tensor (N, C, H, W)
-def preprocess_batch_rand(x, bound_rad, rotating, scaling):
-    batch_size = x.shape[0]
-    input_dim = x.shape[2] * x.shape[3]
-    if rotating:
-        theta_1 = -np.pi + np.random.uniform(size=batch_size) * 2 * np.pi
-        theta_2 = theta_1 - bound_rad + np.random.uniform(size=batch_size) * 2 * bound_rad
-    else:
-        theta_1, theta_2 = np.zeros((batch_size)), np.zeros((batch_size))
-    if scaling:
-        scale_1 = 0.85 + np.random.uniform(size=batch_size) * 0.3
-        scale_2 = scale_1 - 0.15 + np.random.uniform(size=batch_size) * 0.3
-    else:
-        scale_1, scale_2 = np.ones((batch_size), dtype=float), np.ones((batch_size), dtype=float)
-    print(theta_1, theta_2, scale_1, scale_2)
-    x_t = np.reshape(preprocess_batch_det(x, theta_1, scale_1), (batch_size, input_dim))
-    x_next = np.reshape(preprocess_batch_det(x, theta_2, scale_2), (batch_size, input_dim))
-    return torch.FloatTensor(x_t).to(DEVICE), torch.FloatTensor(x_next).to(DEVICE)
 
 # This is for the latent spaces which just need random transformations and want to save the prepro params
 class RandomPreprocessing():
@@ -98,6 +84,7 @@ class RandomPreprocessing():
         x_t = torch.zeros_like(x)
         x_next = torch.zeros_like(x)
         x0, x1 = x.clone().detach(), x.clone().detach()
+        print(self.rotations, self.scaling)
         for i in range(x_t.shape[0]):
             if self.rotations:
                 self.theta_1, self.theta_2 = generate_angles(self.theta_range_1, self.theta_range_2)
